098c7b39220222d3bf8394493bd4ee6fe88f610a
Mathieu Suchet <msuchet63@gmail.com>
2026-05-26 20:23:58 +0200

    Switch to 0.1.3, populate npmVersion

--- generate_api.py
+++ generate_api.py
@@ -6,14 +6,14 @@ import sys
 if __name__ == "__main__":
     print("Deleting directory...")
     try:
-        shutil.rmtree("./generated")
+        shutil.rmtree("./client")
     except (FileNotFoundError, PermissionError):
         pass
     print("Generating")
     subprocess.call([sys.executable, "scripts\\export_openapi.py"])
     os.system(
         "npx @openapitools/openapi-generator-cli generate -i \
-        openapi.json -g typescript-axios -o generated/ \
+        openapi.json -g typescript-axios -o client/ \
         -c typescript-axios-config.yaml"
     )
     # print("Creating .tgz package")

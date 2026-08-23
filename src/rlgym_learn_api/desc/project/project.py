829e951605feda3f693fb908274d4639c1d39cfb
Mathieu Suchet <msuchet63@gmail.com>
2026-08-23 17:23:26 +0200

    Remove entrypoint and add advanced config Pin Axios to 1.18.1

--- src/rlgym_learn_api/desc/project/project.py
+++ src/rlgym_learn_api/desc/project/project.py
@@ -1,10 +1,18 @@
 import os
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
+
+class AdvancedConfigModel(BaseModel):
+    """Optional config the user can tweak at project creation"""
+
+    # Whether the user handles their own venv
+    user_handled_venv: bool = True
 
 class ProjectMetadata(BaseModel):
     name: str
     id: str
     interpreter: str | os.PathLike[str] | None
     created_at_version: str
+
+    advanced_config: AdvancedConfigModel = Field(default_factory=AdvancedConfigModel)
